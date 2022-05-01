c4d87278117335db9613852e7cb9bfc31a514459
Rizka <rizkaarachmawati998@gmail.com>
2022-05-01 21:57:29 +0700

    init commit php bool,number,string

---
+++ TipeDataString.php
@@ -0,0 +1,27 @@
+<?php
+// single quote
+echo 'Name :';
+echo 'Rizka Rachmawati';
+
+// double quote
+echo "\n";
+echo "Name :";
+echo "Rizka \t Rachmawati \n";
+
+// heredoc
+echo <<<RIZKA
+Selamat belajar PHP
+sekarang, kita belajar tipe data string
+ini adalah cara ke-3 membuat string
+bisa menggunakan heredoc
+
+RIZKA;
+
+// nowdoc
+echo <<<'RIZKA'
+Selamat belajar PHP
+sekarang, kita belajar tipe data string
+ini adalah cara ke-3 membuat string
+bisa menggunakan nowdoc
+
+RIZKA;
