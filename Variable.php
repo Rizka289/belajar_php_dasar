6a8f123cfa31d336edeb6dc86536e741a4344038
Rizka <rizkaarachmawati998@gmail.com>
2022-05-02 03:30:19 +0700

    Variabel and Constant

---
+++ Variable.php
@@ -0,0 +1,24 @@
+<?php
+$name = 'Rizka';
+$age = 23;
+
+echo 'Name : ';
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+// variable variables
+$contoh = "eko";
+$$contoh = "kurniawan";
+
+echo "Contoh : ";
+echo $contoh;
+echo "\n";
+
+echo "Eko : ";
+echo $eko;
+echo "\n";
+
