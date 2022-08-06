0eb3c8471894007d427ea770a554217be9eeec29
Rizka <rizkaarachmawati998@gmail.com>
2022-08-06 22:00:17 +0700

    komentar dan string function

---
+++ Komentar.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ ini adalah komentar panjang
+ njnaksd
+*/
+
+function sayHello()
+{
+    // komentar satu baris
+    echo "Hello Komentar" . PHP_EOL;
+}
+sayHello();
---
+++ StringFunction.php
@@ -0,0 +1,7 @@
+<?php
+var_dump(join("," , [10,11,12,13,14,15]));
+var_dump(explode(" ", "Rizka Rachmawati"));
+var_dump(strtolower("RIZKA RACHMAWATI"));
+var_dump(strtoupper("rizka rachmawati"));
+var_dump(trim("      Rizka      "));
+var_dump(substr("Rizka Rachmawati", 0,3));
