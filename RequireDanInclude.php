f1c27edfc94f0914c55b36741c1dcdabc9a26b0f
Rizka <rizkaarachmawati998@gmail.com>
2022-08-08 14:30:42 +0700

    array function,is function, require dan include

---
+++ RequireDanInclude.php
@@ -0,0 +1,8 @@
+<?php
+// require "Lib/MyFunction.php";
+// include "Lib/MyFunction.php";
+// include "Lib/MyFunction.php";
+include_once "Lib/MyFunction.php";
+include_once "Lib/MyFunction.php";
+
+echo sayHello("Rizka", "rachmawati");
